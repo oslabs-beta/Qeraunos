45f7d5172ca2c2e6a0417b83f23205f9a47f4274
Dennis Cheung <dennis@Denniss-MacBook-Air.local>
2023-01-03 17:13:48 -0800

    fixes .env PG_URI import

--- server/models/starwarsModel.js
+++ server/models/starwarsModel.js
@@ -1,8 +1,8 @@
 const { Pool } = require('pg');
+require('dotenv').config();
 // create a new pool here using the connection string above
 const pool = new Pool({
-  connectionString:
-    'postgres://hfupnoff:aVwC3zy9Ue2kTYAKAFJAzH9T1wt-6kWU@heffalump.db.elephantsql.com/hfupnoff',
+  connectionString: process.env.PG_URI,
 });
 
 module.exports = {
