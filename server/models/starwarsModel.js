32e50d615c20c473529549e73e7f4a1d84a83a96
Dennis Cheung <dennis@Denniss-MacBook-Air.local>
2023-01-03 17:00:06 -0800

    added graphql schema

--- server/models/starwarsModel.js
+++ server/models/starwarsModel.js
@@ -1,7 +1,8 @@
 const { Pool } = require('pg');
 // create a new pool here using the connection string above
 const pool = new Pool({
-  connectionString: process.env.PG_URI,
+  connectionString:
+    'postgres://hfupnoff:aVwC3zy9Ue2kTYAKAFJAzH9T1wt-6kWU@heffalump.db.elephantsql.com/hfupnoff',
 });
 
 module.exports = {
