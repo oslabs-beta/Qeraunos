cb7d8a258678c39ce0d846b4f60eb2c978c95f3c
Arthur Huynh <arthurhuynh@ArthursComputer.local>
2023-01-05 07:54:49 -0800

    added new file for LFU caching algo

--- caching/LFU-caching.js
+++ caching/LFU-caching.js
@@ -1,129 +1,155 @@
 function LfuNode(key, value) {
-  this.next = null;
   this.prev = null;
+  this.next = null;
   this.key = key;
   this.value = value;
   this.freqCount = 1;
 }
 
-function LfuDLL(freqCount) {
-  this.freqCount = freqCount;
-  this.head = null;
-  this.tail = null;
+function LfuDLL() {
+  this.head = new LfuNode('head', null);
+  this.tail = new LfuNode('tail', null);
+  this.head.next = this.tail;
+  this.tail.prev = this.head;
+  this.size = 0;
 }
 
-const LfuCache = function (capacity) {
-  this.keys = {};
-  this.frequency = {};
-  this.capacity = capacity;
-  this.minFrequency = 0;
-  this.size = 0;
-};
+LfuDLL.prototype.insertAtHead = function (node) {
+  // set current head as new node's next
+  node.next = this.head.next;
+  this.head.next.prev = node;
 
-LfuCache.prototype.removeAtTail = function (node) {
-  // delete from hashtable
-  delete this.keys[node.tail.key];
-  // delete from linked list
-  // if there is only one node in list
-  if (node.head == node.tail) {
-    node.head = null;
-    node.tail = null;
-    // if there are more than 1 node, set tail to previous and its next to null to delete
-  } else {
-    node.tail = node.tail.prev;
-    node.tail.next = null;
-  }
-  this.size--;
-};
+  // set current head as new node
+  this.head.next = node;
+  node.prev = this.head;
 
-// creates a frequency list if not already existing and inserts a new node into it
-LfuCache.prototype.addNode = function (key, value) {
-  let node = new LfuNode(key, value);
-  // place node into hashtable
-  this.keys[key] = node;
-  // check if frequency of 1 exists already, if not, create a new list with freq of 1
-  if (!this.frequency[1]) this.frequency[1] = new LfuDLL(1);
-  // insert the new into the freq list using helper function
-  this.insertAtHead(node, this.frequency[1]);
-  this.minFrequency = 1;
   this.size++;
 };
 
-// inserts a new node at the head
-LfuCache.prototype.insertAtHead = function (newNode, freqList) {
-  let head = freqList.head;
-  // if there are no nodes in freq list, head and tail of list are the new node
-  if (head === null) {
-    freqList.head = newNode;
-    freqList.tail = newNode;
-    return;
-  }
-  // sets newNode at the head by setting current heads prev to point at newNode and newNode to point at heads next
-  head.prev = newNode;
-  newNode.next = head;
-  newNode.prev = null;
-  // sets the head of the freq list to the newNode with the new pointers
-  freqList.head = newNode;
+LfuDLL.prototype.removeAtTail = function () {
+  // save last node to return
+  const oldTail = this.tail.prev;
+
+  // get reference to node we want to remove
+  const prev = this.tail.prev;
+
+  prev.prev.next = this.tail;
+  this.tail.prev = prev.prev;
+
+  this.size--;
+  return oldTail;
 };
 
-// updates node when get or set is used
-LfuCache.prototype.updateNode = function (node) {
-  let freq = node.freqCount;
-  node.freqCount = freq + 1;
-  // removes node from frequency list using helper function
-  this.removeNode(node, this.frequency[freq]);
-  // if the minimum frequency is equal to the frequency of the node AND the hashtable does not have a node at that frequency,
-  // set the min frequency to the nodes freqCount
-  if (this.minFrequency == freq && this.frequency[freq].head == null)
-    this.minFrequency = node.freqCount;
-  // if the frequency at the nodes freqCount does not exist in the hash table, set the freq hashtable with a new linked list at that freqCount
-  if (!this.frequency[node.freqCount])
-    this.frequency[node.freqCount] = new LfuDLL(node.freqCount);
-  // resets node
-  node.next = null;
-  node.prev = null;
-  // inserts reset node into freq hashtable at its freqCount
-  this.insertAtHead(node, this.frequency[node.freqCount]);
+LfuDLL.prototype.removeNode = function (node) {
+  node.prev.next = node.next;
+  node.next.prev = node.prev;
+  this.size--;
 };
 
-// removes node from linked freq list
-LfuCache.prototype.removeNode = function (newNode, freqList) {
-  let prev = newNode.prev;
-  let next = newNode.next;
-  if (prev) prev.next = next;
-  if (next) next.prev = prev;
-  // check if newNode is either at the head or tail, and set the head or tail of freq list accordingly
-  if (newNode == freqList.head) freqList.head = next;
-  if (newNode == freqList.tail) freqList.tail = prev;
-};
+function LfuCache(capacity) {
+  // stores LfuNodes
+  this.keys = {};
 
-// O(1) get function to get node from key
-LfuCache.prototype.get = function (key) {
-  if (!this.keys[key]) return 'data does not exist in cache';
-  // get, update, return the nodes freqCount
-  let node = this.keys[key];
-  this.updateNode(node);
-  return node.value;
-};
+  // stores LfuDDL
+  this.frequency = {};
 
-// O(1) set function to set new node
+  this.capacity = capacity;
+
+  // keeps track of the lowest frequency linked list
+  this.minFrequency = 0;
+
+  this.size = 0;
+}
+
 LfuCache.prototype.set = function (key, value) {
-  // check if node is already in hashtable
-  if (this.keys[key]) {
-    let node = this.keys[key];
-    node.value = value;
-    this.updateNode(node);
-  } else if (this.size < this.capacity) {
-    // check if current size of hashtable is less than the capacity constraint and insert a new node
-    this.addNode(key, value);
-  } else if (this.capacity > 0) {
-    // otherwise if size is greater than or equal to capacity, and capacity is greater than 0,
-    // remove the least frequently occurring data from the tail, which will also be the least recently used one
-    let leastFreqNode = this.frequency[this.minFrequency];
-    this.removeAtTail(leastFreqNode);
-    // after removing the tail to make room, add the new node
-    this.addNode(key, value);
+  let node = this.keys[key];
+  // if node doesnt exit in keys, then add it
+  if (node === undefined) {
+    // create new node and store in keys
+    node = new LfuNode(key, value);
+    this.keys[key] = node;
+    node.data = value;
+
+    // if there is space for the node, add it to the linked list with frequency of 1
+    if (this.size !== this.capacity) {
+      // if linked list for frequency 1 does not exist then create it
+      if (this.frequency[1] === undefined) {
+        this.frequency[1] = new LfuDLL();
+      }
+
+      // add new node and increment size of frequency 1
+      this.frequency[1].insertAtHead(node);
+      this.size++;
+
+      // if frequency 1 is full and we need to deleted a node, delete the tail
+    } else {
+      const oldTail = this.frequency[this.minFrequency].removeAtTail();
+      delete this.keys[oldTail.key];
+
+      // if we deleted frequency 1 then add it back before adding new node
+      if (this.frequency[1] === undefined) {
+        this.frequency[1] = new LfuDLL();
+      }
+      this.frequency[1].insertAtHead(node);
+    }
+    this.minFrequency = 1;
+
+    // if the node exists, we need to get it and move it to the new linked list
+  } else {
+    // save the old frequency of the node and increment and update the data
+    const oldFreqCount = node.freqCount;
+
+    node.data = value;
+    node.freqCount++;
+
+    // remove node from linked list
+    this.frequency[oldFreqCount].removeNode(node);
+
+    // if new list does not exit then create it
+    if (this.frequency[node.freqCount] === undefined) {
+      this.frequency[node.freqCount] = new LfuDLL();
+    }
+
+    // add new node to new linked list with the incremented freqcount
+    this.frequency[node.freqCount].insertAtHead(node);
+
+    // if the node we incremented was in the minFrequency list
+    // and there is nothing left in the old list, then we know
+    // the new minFrequency for any node in any list is the new
+    // freq so we can increment that now
+    if (
+      oldFreqCount == this.minFrequency &&
+      Object.keys(this.frequency[oldFreqCount]).size === 0
+    ) {
+      this.minFrequency++;
+    }
   }
+};
+
+LfuCache.prototype.get = function (key) {
+  const node = this.keys[key];
+  if (node === undefined) return null;
+
+  const oldFreqCount = node.freqCount;
+  node.freqCount++;
+
+  // remove node from old frequency list and create new one if next one doesnt exist
+  // before adding the neow to the next list at the head
+  this.frequency[oldFreqCount].removeNode(node);
+  if (!this.frequency[node.freqCount]) {
+    this.frequency[node.freqCount] = new LfuDLL();
+  }
+
+  this.frequency[node.freqCount].insertAtHead(node);
+
+  // if old frequency list is empty then update minfrequency
+  if (
+    oldFreqCount === this.minFrequency &&
+    Object.keys(this.frequency[oldFreqCount]).length === 0
+  ) {
+    this.minFrequency++;
+  }
+  return node.data;
 };
 
 module.exports = LfuCache;
