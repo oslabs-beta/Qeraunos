4c1824ccd3e13415e9c24c8ea4cb1cd034ef748c
Amrit Ramos <amrit.ramos@gmail.com>
2023-01-09 18:22:22 -0300

    Front end with all the stretch feature components laid out. Nav Bar also fully functioning

--- client/App.jsx
+++ client/App.jsx
@@ -1,4 +1,6 @@
+//importing structure
 import React from 'react';
+import { Route, Routes } from 'react-router-dom';
 
 import {
   RecoilRoot,
@@ -7,21 +9,31 @@ import {
   useRecoilState,
   useRecoilValue,
 } from 'recoil';
+
+//importing styling and images
 import './stylesheets/styles.scss';
-import Sidebar from './container/Sidebar.jsx';
-import DataVisualizer from './container/DataVisualizer.jsx';
 import whiteLogo from './resources/logo-white.png';
 import blueLogo from './resources/logo-blue.png';
+
+//importing components
+import Demo from './pages/Demo';
+import Home from './pages/Home';
+import Landing from './pages/Landing';
+import Team from './pages/Team';
+import Navbar from './container/Navbar';
+import Installation from './pages/Installation';
 
 const App = () => {
   return (
     <RecoilRoot>
-      <div>
-        <img src={whiteLogo} alt='Qeraunos Logo' className='logo' />
-      </div>
-      <div className='main-display'>
-        <Sidebar />
-        <DataVisualizer />
+      <Navbar />
+      <div className="container">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/demo" element={<Demo />} />
+          <Route path="/team" element={<Team />} />
+          <Route path="/installation" element={<Installation />} />
+        </Routes>
       </div>
     </RecoilRoot>
   );
---
+++ client/pages/Landing.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Landing() {
+  return (
+    <div>
+      <h2>Landing</h2>
+    </div>
+  );
+}
+
+export default Landing;
