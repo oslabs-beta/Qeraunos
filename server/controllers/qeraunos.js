0abbb0665072566ffcacfec1eb008a0eb66d3712
Arthur Huynh <arthurhuynh@Arthurs-Computer.local>
2023-01-11 21:23:38 -0500

    adds full functional mutation caching ability

--- server/controllers/qeraunos.js
+++ server/controllers/qeraunos.js
@@ -7,7 +7,7 @@ const { query } = require('express');
 function Qeraunos(schema) {
   this.schema = schema;
   this.query = this.query.bind(this);
-  this.mutation = this.mutation.bind(this);
+  // this.mutation = this.mutation.bind(this);
 }
 
 const newLfu = new LfuCache(100);
@@ -67,33 +67,79 @@ Qeraunos.prototype.query = async function (req, res, next) {
   const keyword = graphqlParser(this.schema, req.body.query);
   const operation = parse(req.body.query).definitions[0].operation;
   // check if its a mutation, if it is, pass over function to mutation
-  if (operation === 'mutation') {
-    console.log('in query method, has mutation, pass over to mutation');
-    return next();
-  }
 
   try {
-    console.log('In query');
-    // check whether key exists in cache, if so return value from cache.
-    // if not, send a graphQL query
-    if (newLfu.keys[keyword]) {
-      // console.log('newLfu.keys[key]: ', newLfu.keys[key]);
-      // console.log('in cache');
-      res.locals.graphql = newLfu.get(keyword);
-      res.locals.response = 'Cached';
-      console.log('OLD CACHE OBJ IN QUERY', newLfu.keys);
-      return next();
-    } else {
+    if (operation === 'mutation') {
+      console.log('in query method, has mutation, pass over to mutation');
+      // check if query is a mutation type, if not, move on
+      if (operation !== 'mutation') {
+        console.log('in mutation method, does not have mutation, move on');
+        return next();
+      }
+      console.log('in mutation');
+      // // need to loop through cache and check any key with type and id
+      // split keyword into its useful strings
+      const searchArr = keyword.split('.');
+      // this string holds the type of the graphql query
+      const searchType = searchArr[0];
+      // this string combines the type and the id to search the cache keys with
+      const searchKey = searchArr[0] + '.' + searchArr[1];
+      // this part of the string holds the actual graph ql query itself
+      const graphqlQuery = searchArr[2];
+      // loop through cache and check to see if mutated data point is referenced in any of the keys
+      // if so, manually update that key with a new graphql query.
+      // also check for any multiple types held in brackets and update those as well since they will hold
+      // the individual data point in its value
+      console.log('==========UPPERCUT==========');
+      console.log(newLfu.keys);
+      for (const key in newLfu.keys) {
+        console.log('mutation searchKey: ', searchKey);
+        console.log('mutation key in cache: ', key);
+        console.log('mutation keyword: ', keyword);
+        if (key.includes(searchKey) || key.includes(`[${searchType}]`)) {
+          console.log('==========HIT==========');
+          console.log('searchkey: ', searchKey);
+          console.log('searchType: ', `[${searchType}]`);
+          await graphql({
+            schema: this.schema,
+            source: graphqlQuery,
+          });
+        }
+      }
       const data = await graphql({
         schema: this.schema,
         source: req.body.query,
       });
       res.locals.graphql = data;
-      // console.log('data', data);
-      res.locals.response = 'Uncached';
+      res.locals.response = 'UNCACHED';
+      // set new mutation in cache
+      // do we need to do this?
       newLfu.set(keyword, data);
-      console.log('NEW CACHE OBJ IN QUERY', newLfu.keys);
+      console.log('NEW CACHE OBJ IN MUTATION', newLfu.keys);
       return next();
+    } else {
+      console.log('In query');
+      // check whether key exists in cache, if so return value from cache.
+      // if not, send a graphQL query
+      if (newLfu.keys[keyword]) {
+        // console.log('newLfu.keys[key]: ', newLfu.keys[key]);
+        // console.log('in cache');
+        res.locals.graphql = newLfu.get(keyword);
+        res.locals.response = 'Cached';
+        console.log('OLD CACHE OBJ IN QUERY', newLfu.keys);
+        return next();
+      } else {
+        const data = await graphql({
+          schema: this.schema,
+          source: req.body.query,
+        });
+        res.locals.graphql = data;
+        // console.log('data', data);
+        res.locals.response = 'Uncached';
+        newLfu.set(keyword, data);
+        console.log('NEW CACHE OBJ IN QUERY', newLfu.keys);
+        return next();
+      }
     }
   } catch (err) {
     return next({
@@ -103,56 +149,9 @@ Qeraunos.prototype.query = async function (req, res, next) {
   }
 };
 
-Qeraunos.prototype.mutation = async function (req, res, next) {
-  // check if query is a mutation type, if not, move on
-  const operation = parse(req.body.query).definitions[0].operation;
-  if (operation !== 'mutation') {
-    console.log('in mutation method, does not have mutation, move on');
-    return next();
-  }
-  console.log('in mutation');
-  // // need to loop through cache and check any key with type and id
-  const keyword = graphqlParser(this.schema, req.body.query);
-  // split keyword into its useful strings
-  const searchArr = keyword.split('.');
-  // this string holds the type of the graphql query
-  const searchType = searchArr[0];
-  // this string combines the type and the id to search the cache keys with
-  const searchKey = searchArr[0] + '.' + searchArr[1];
-  // this part of the string holds the actual graph ql query itself
-  const graphqlQuery = searchArr[2];
-  // loop through cache and check to see if mutated data point is referenced in any of the keys
-  // if so, manually update that key with a new graphql query.
-  // also check for any multiple types held in brackets and update those as well since they will hold
-  // the individual data point in its value
-  console.log('==========UPPERCUT==========');
-  console.log(newLfu.keys);
-  for (const key in newLfu.keys) {
-    console.log('mutation searchKey: ', searchKey);
-    console.log('mutation key in cache: ', key);
-    console.log('mutation keyword: ', keyword);
-    if (key.includes(searchKey) || key.includes(`[${searchType}]`)) {
-      console.log('==========HIT==========');
-      console.log('searchkey: ', searchKey);
-      console.log('searchType: ', `[${searchType}]`);
-      await graphql({
-        schema: this.schema,
-        source: graphqlQuery,
-      });
-    }
-  }
-  const data = await graphql({
-    schema: this.schema,
-    source: req.body.query,
-  });
-  res.locals.graphql = data;
-  res.locals.response = 'UNCACHED';
-  // set new mutation in cache
-  // do we need to do this?
-  newLfu.set(keyword, data);
-  console.log('NEW CACHE OBJ IN MUTATION', newLfu.keys);
-  return next();
-};
+// Qeraunos.prototype.mutation = async function (req, res, next) {
+
+// };
 
 // converts query from client into a unique key by removing extraneous symbols into a single string
 // const keyConverter = (query) => {
