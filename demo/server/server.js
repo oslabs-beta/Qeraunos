4419b0f6705e7ccf406c0a3098dbc0761512e30f
Arthur Huynh <arthurhuynh@Arthurs-Computer.local>
2023-01-24 15:59:33 -0500

    cleaned up server file

--- demo/server/server.js
+++ demo/server/server.js
@@ -3,6 +3,7 @@ exports.__esModule = true;
 var express = require('express');
 var path = require('path');
 var schema = require('./schema/schema');
+//made custom reset qeraunos so that our server would reset every hour for demo purposes. Should require package here.
 var Qeraunos = require('./qeraunos.js');
 var app = express();
 var PORT = 3000;
@@ -12,15 +13,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use('/', express.static(path.resolve(__dirname, '../client')));
 //pass in graphQL schema (mandatory) as well as Redis acct info (optional if you want to use Redis)
-// const qeraunos = new Qeraunos(schema, '127.0.0.1', '6379');
+// const qeraunos = new Qeraunos(schema, RedisHost, RedisPort, RedisPassword);
 var qeraunos = new Qeraunos(schema);
 qeraunos.setSize(100);
-// setInterval(() => {
-//   qeraunos = new Qeraunos(schema); 
-//   qeraunos.setSize(100)
-//   console.log("this is hitting every 10 seconds")
-//   console.log('This is new qeraunos instance', qeraunos.qeraunosCache)
-// }, 10*1000)
 app.use('/graphql', qeraunos.query, function (req, res) {
     return res.status(200).send(res.locals);
 });
@@ -31,12 +26,8 @@ expressGraphQL({
     schema: schema,
     graphiql: true
 }));
-// // deletes cache and spins up a new instance
-// app.use('/clearCache', qeraunos.query, (req: Request, res: Response) => {
-// });
 // 404 error handler
 app.use(function (req, res) {
-    console.error('Server.js 404');
     return res.sendStatus(404);
 });
 //Global error handler
@@ -47,7 +38,6 @@ app.use(function (err, req, res, next) {
         message: { err: 'An error occurred' }
     };
     var errorObj = Object.assign({}, defaultErr, err);
-    console.log(errorObj.log);
     return res.status(errorObj.status).json(errorObj.message);
 });
 app.listen(PORT, function () {
--- demo/server/server.ts
+++ demo/server/server.ts
@@ -2,7 +2,7 @@ const express = require('express');
 import { Request, Response, NextFunction } from 'express';
 const path = require('path');
 const schema = require('./schema/schema');
-//made custom reset qeraunos so that our server would reset every hour for demo purposes. 
+//made custom reset qeraunos so that our server would reset every hour for demo purposes. Should require package here.
 const Qeraunos = require('./qeraunos.js');
 const app = express();
 const PORT = 3000;
@@ -20,16 +20,9 @@ app.use(express.urlencoded({ extended: true }));
 app.use('/', express.static(path.resolve(__dirname, '../client')));
 
 //pass in graphQL schema (mandatory) as well as Redis acct info (optional if you want to use Redis)
-// const qeraunos = new Qeraunos(schema, '127.0.0.1', '6379');
+// const qeraunos = new Qeraunos(schema, RedisHost, RedisPort, RedisPassword);
 let qeraunos = new Qeraunos(schema);
 qeraunos.setSize(100);
-
-// setInterval(() => {
-//   qeraunos = new Qeraunos(schema); 
-//   qeraunos.setSize(100)
-//   console.log("this is hitting every 10 seconds")
-//   console.log('This is new qeraunos instance', qeraunos.qeraunosCache)
-// }, 10*1000)
 
 app.use('/graphql', qeraunos.query, (req: Request, res: Response) => {
   return res.status(200).send(res.locals);
@@ -45,14 +38,8 @@ app.use(
   })
 );
 
-// // deletes cache and spins up a new instance
-// app.use('/clearCache', qeraunos.query, (req: Request, res: Response) => {
-
-// });
-
 // 404 error handler
 app.use((req: Request, res: Response) => {
-  console.error('Server.js 404');
   return res.sendStatus(404);
 });
 
@@ -64,7 +51,6 @@ app.use((err: ServerError, req: Request, res: Response, next: NextFunction) => {
     message: { err: 'An error occurred' },
   };
   const errorObj: ServerError = Object.assign({}, defaultErr, err);
-  console.log(errorObj.log);
   return res.status(errorObj.status).json(errorObj.message);
 });
 
